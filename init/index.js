90692a7691fa96c8510944c03bc13f78258ec631
MOHAMMAD FAIZAN <md faizan>
2026-07-13 16:52:15 +0530

    fake data initialized

--- init/index.js
+++ init/index.js
@@ -2,9 +2,21 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
+if (process.env.NODE_ENV != "production") {
+    require('dotenv').config({ path: __dirname + '/../.env' });
+}
 
-main()
+// Top of app.js ya server file mein
+const dns = require('dns');
+dns.setServers(['1.1.1.1', '1.0.0.1']);   // Cloudflare DNS (SRV records achhe se handle karta hai)
+
+// const MONGO_URL = "mongodb://127.0.0.1:27017/realwonderlust";
+
+
+
+const dbUrl = process.env.ATLASDB_URL;
+
+main() 
   .then(() => {
     console.log("connected to DB");
   })
@@ -13,13 +25,12 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 const initDB = async () => {
-  await Listing.deleteMany({});
-  initData.data = initData.data.map((obj)=>({ ...obj, owner: "69a5c6d89a4035f1999d668e",
-  }));
+  // await Listing.deleteMany({});
+  initData.data = initData.data.map((obj)=>({ ...obj, owner: "69a5c6d89a4035f1999d668e",}));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");  
 };
